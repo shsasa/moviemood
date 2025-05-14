bdc3f378814de95f8ac26f758aa5f4c9b1782bac
Bodoor <Bodoor5@hotmail.com>
2025-05-14 14:19:16 +0300

    adding comments to server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
 const app = express()
-
-
+const path = require('path')
+//Middlewares
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const morgan = require('morgan')
@@ -11,15 +11,28 @@ const session = require('express-session')
 const passUserToView = require('./middleware/pass-user-to-view')
 const isSignedIn = require('./middleware/is-signed-in')
 
-
+// Set the port to 3000
 const port = process.env.PORT ? process.env.PORT : '3000'
 mongoose.connect(process.env.MONGODB_URI)
-
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
 })
 
+app.use(passUserToView)
+
+// Middleware to parse URL-encoded data from forms (Body Parser)
+app.use(express.urlencoded({ extended: false }))
+
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride('_method'))
+
+// Morgan for logging HTTP requests
+app.use(morgan('dev'))
+
+//for css
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Session configurations
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -27,24 +40,19 @@ app.use(
     saveUninitialized: true
   })
 )
-app.use(passUserToView)
 
-
+//Require Controller
 const authController = require('./controllers/auth')
 const usersController = require('./controllers/users')
-app.use(express.urlencoded({ extended: false }))
-
-app.use(methodOverride('_method'))
-app.use(morgan('dev'))
 app.use('/auth', authController)
 app.use('/users', usersController)
 
+//GET METHOD
 app.get('/', async (req, res) => {
   res.render('index.ejs')
 })
 
-
-
+//listen to port
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`)
 })
