f32f7da62a7fc8b063c5e10b16d2683b08eb137a
Bodoor <Bodoor5@hotmail.com>
2025-05-15 12:22:23 +0300

    added lists.js

---
+++ controllers/lists.js
@@ -0,0 +1,8 @@
+//controllers
+const router = require('express').Router()
+const Movie = require('../models/movie')
+const axios = require('axios')
+const User = require('../models/user')
+const List = require('../models/list')
+
+module.exports = router
