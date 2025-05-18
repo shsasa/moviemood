9f9f0a036f0b824aaea4b0861e77ddc91273ca04
naimjalal <naimjalal@gmail.com>
2025-05-18 12:49:56 +0300

    users upload pic

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/user')
 const isSignedIn = require('../middleware/is-signed-in');
-
+const upload= require('../middleware/upload')
 
 router.get('/', async (req, res) => {
   const users = await User.find()
@@ -22,19 +22,24 @@ router.get("/:id/edit", isSignedIn, async (req,res)=>{
 })
 
 
-router.put("/:id", isSignedIn, async (req, res) => {
+router.put("/:id", isSignedIn, upload.single("profileImage"), async (req, res) => {
   if (req.params.id !== req.session.user._id.toString()) {
     return res.send("Unauthorized");
   }
-  const { username, email, image } = req.body;
-  const updatedUser =await User.findByIdAndUpdate(req.params.id, {
-    username,
-    email,
-    image: image?.trim() || undefined
-  },
+  const { username, email} = req.body;
+  const updatedFields ={username,email,}
+  if(req.file){
+    updatedFields.image=`/uploads/${req.file.filename}`
+  }
+  const updatedUser =await User.findByIdAndUpdate(req.params.id,
+    updatedFields,
   {new : true}
 );
 req.session.user.username = updatedUser.username
-  res.redirect(`/users/${req.params.id}`);
+  // res.redirect(`/users/${req.params.id}`);
+  if(updatedUser.image){
+    req.session.user.image = updatedUser.image
+  }
+  res.redirect(`/users/${req.params.id}`)
 });
 module.exports = router
---
+++ middleware/upload.js
@@ -0,0 +1,6 @@
+const multer = require("multer")
+
+const upload = multer({dest:"public/uploads"})
+
+
+module.exports = upload
